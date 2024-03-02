6f72d1fba2a05ccdf9bf1c9dc4a4ea35ef95b093
Abhik2005 <142106053+Abhik2005@users.noreply.github.com>
2024-03-02 06:32:48 +0000

    Create a simple useState project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import './App.css'
+import { useState } from "react";
 
-export default function App() {
+function App() {
+  var [counter, setCounter] = useState(0);
+  var addvalue = () => {
+    if (counter >= 20) {
+      setCounter(20);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
+  var removevalue = () => {
+    if (counter <= 0) {
+      setCounter(0);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-  )
+    <>
+      <h1>Abhik Mondal </h1>
+      <button onClick={addvalue}>Add value : {counter}</button>
+
+      <br />
+      <button onClick={removevalue}>Remove value : {counter}</button>
+    </>
+  );
 }
+
+export default App;
---
+++ src/abhik.jsx
@@ -0,0 +1,7 @@
+function Abhik() {
+   return(
+     <h1>Hello from Abhik</h1>
+   )
+}
+
+export default Abhik;
